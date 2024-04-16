4134e08a23b7c1964a92939037675fed41361257
FabioNandes <fabionandesrj@gmail.com>
2024-04-16 17:18:24 -0300

    Alterando configuração do cypress

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,13 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      const { defineConfig } = require('cypress')
+
+      module.exports = defineConfig({
+        video: true,
+        videoCompression: true,
+      })
+
     },
   },
 });
